13ac2a374ee971e05e2e7d3aac231a40b1f8bdcb
AndreaCuriel <acurielipn@hotmail.com>
2022-04-21 12:50:26 -0500

    Crear clase spiderman

---
+++ spiderverse/app/spiderman.js
@@ -0,0 +1,11 @@
+class Spiderman {
+  constructor(name, age, actor, movies, studies) {
+    this.name = name;
+    this.age = age;
+    this.actor = actor;
+    this.movies = movies;
+    this.studies = studies;
+  }
+}
+
+module.exports = Spiderman;
