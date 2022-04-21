9e4f042c58300c72cc6dba07b4b86e132cd4a961
AndreaCuriel <acurielipn@hotmail.com>
2022-04-21 13:16:01 -0500

    Hacer fallar la prueba, al no existir aún la prueba

--- spiderverse/test/spiderman.test.js
+++ spiderverse/test/spiderman.test.js
@@ -1,7 +1,8 @@
+const { createTestScheduler } = require("jest");
 const Spiderman = require("./../app/spiderman");
 
 describe("Unit tests for Spiderman", () => {
-  test("Create spiderman objet", () => {
+  test("1) Create spiderman objet", () => {
     //Intanciar el objeto
     const myspiderman = new Spiderman("Spiderman", 35, "Andrew", 5, "Sony");
 
@@ -11,5 +12,18 @@ describe("Unit tests for Spiderman", () => {
     expect(myspiderman.actor).toBe("Andrew");
     expect(myspiderman.movies).toBe(5);
     expect(myspiderman.studies).toBe("Sony");
+  });
+
+  test("2 Use the method getInfo()", () => {
+    const tomholland = new Spiderman(
+      "Spiderman Marvel",
+      25,
+      "Tom Holland",
+      5,
+      "Marvel"
+    );
+    expect(tomholland.getInfo()).toBe(
+      "Hey, I'm Tom Holland from Marvel studios"
+    );
   });
 });
